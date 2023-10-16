3105936c85b44c01f64c398eb0e6b0d621c7f4c2
djh20 <danmanmrdj@gmail.com>
2023-10-17 11:41:49 +1300

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
